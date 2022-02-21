4ce1867357fcfb7458cfd6a7d902bc617002dc11
Antony Barbosa <antonybr@live.com>
2022-02-21 17:01:40 -0300

    first server

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+const http  = require('http')
 var  cron  =  require ( 'node-cron' ) ;
+const hostname = "localhost";
+const port = 3333;
+
 
 const farmina = require('./src/farmina/envioFarmina')
 const cafune = require('./src/cafune/envioCafune')
@@ -25,9 +29,6 @@ function enviarsell(value){
 }
 
 
-
-
-
 function agenda(funcao, min, hr, value){
     
     var tk = cron.schedule ( `${min} ${hr} * * *` ,  ( )  =>  { 
@@ -50,4 +51,16 @@ agenda(enviarsell, '57', '17',3)
 agenda(enviarsell, '58', '17',4)
 agenda(enviarsell, '59', '17',5)
 
+const server = http.createServer((req, res)=>{
+    res.setHeader("Content-type", "text-plain"),
+    res.end("hello world")
+})
 
+server.listen(port, hostname, ()=>{
+    agenda(enviarsell, '55', '17',1)
+    agenda(enviarsell, '56', '17',2)
+    agenda(enviarsell, '55', '17',3)
+    agenda(enviarsell, '58', '17',4)
+    agenda(enviarsell, '59', '17',5)
+    console.log("Servidor rodando")
+})
